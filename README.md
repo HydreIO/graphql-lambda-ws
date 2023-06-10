8308ac2fe48758cee9011c58884d3caccc928c86
Sceat <fetch@sceat.xyz>
2023-06-10 13:48:13 -0600

    feat: init repo

---
+++ README.md
@@ -0,0 +1,53 @@
+# @hydre/graphql-lambda-ws 🌐🚀
+
+[![NPM version](https://img.shields.io/npm/v/hydre/graphql-lambda-ws.svg?style=flat-square)](https://www.npmjs.com/package/graphql_serverless_websocket)
+[![Downloads](https://img.shields.io/npm/dm/@hydre/graphql-lambda-ws.svg?style=flat-square)](https://www.npmjs.com/package/graphql_serverless_websocket)
+
+Welcome to GraphQL Serverless WebSocket library, a simple and powerful solution for implementing GraphQL subscriptions over WebSocket on Serverless architectures. 🛰️🌟
+
+## Features ✨
+
+- **AWS Integration**: Built-in integration with AWS API Gateway Management API for easy deployment on AWS Lambda.
+- **Flexible Error Handling**: Customize how errors are formatted and returned to the client.
+- **Built-In Validation**: Automatically validates incoming GraphQL requests against your schema.
+
+## Installation 📦
+
+Use the package manager [npm](https://www.npmjs.com/) to install GraphQL Serverless WebSocket Library.
+
+```bash
+npm install @hydre/graphql-lambda-ws
+```
+
+## Usage 🚀
+
+This library exposes a single function that returns a new server instance. This function takes an options object with the following properties:
+
+The returned server instance is a function that you can use as your AWS Lambda handler.
+
+Here is a simple usage example:
+
+```javascript
+import serve from '@hydre/graphql-lambda-ws'
+import make_schema from '@hydre/make_schema'
+
+// this returns a function (event, context) => {}
+export default serve({
+  // the build schema
+  schema: make_schema('type Query {}'),
+  // An async function that builds the context for each request.
+  build_context: async ({ event, context }) => ({}),
+  // The root value for your resolvers
+  root_value: {},
+  // Options for the AWS API Gateway Management API client.
+  aws_client_options: {},
+  // A function that formats errors before they're returned to the client.
+  format_error: error => error,
+});
+```
+
+## Contributing 🤝
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to test your changes
---
+++ index.js
@@ -0,0 +1,90 @@
+import {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} from '@aws-sdk/client-apigatewaymanagementapi'
+import { aiter } from 'iterator-helper'
+import {
+  parse,
+  getOperationAST,
+  execute,
+  subscribe,
+  validate,
+  ExecutionResult,
+} from 'graphql'
+
+export default ({
+  schema,
+  build_context,
+  rootValue,
+  aws_client_options = {},
+  format_error = error => error,
+}) => {
+  const client = new ApiGatewayManagementApiClient(aws_client_options)
+
+  const format_body = ({ data, errors = [] } = {}) =>
+    JSON.stringify({
+      data,
+      errors: errors.map(format_error),
+    })
+
+  const Result = {
+    success: body => {
+      return {
+        type: 'application/json',
+        statusCode: 200,
+        body: format_body(body),
+      }
+    },
+    failure: errors => ({
+      data: undefined,
+      // errors might not be an array
+      errors: [errors].flatMap(format_error),
+    }),
+  }
+
+  return async (event, context) => {
+    const {
+      body,
+      requestContext: { connectionId },
+    } = event
+    const { query, operationName, variables } = JSON.parse(body)
+
+    if (!query) return Result.failure(new Error("'query' field not provided"))
+
+    const document = parse(query)
+    const errors = validate(schema, document)
+
+    if (errors.length) return Result.failure(errors)
+
+    const { operation } = getOperationAST(document, operationName)
+    if (!operation)
+      return Result.failure(new Error(`Operation '${operationName}' not found`))
+
+    const options = {
+      document,
+      schema,
+      operationName,
+      rootValue,
+      variableValues: variables,
+      contextValue: (await build_context({ event, context })) ?? {},
+    }
+
+    if (operation === 'subscription') {
+      const subscribe_result = await subscribe(options)
+      if (subscribe_result instanceof ExecutionResult)
+        return Result.success(subscribe_result)
+
+      await aiter(await subscribe_result()).forEach(result =>
+        client.send(
+          new PostToConnectionCommand({
+            ConnectionId: connectionId,
+            Data: format_body(result),
+          })
+        )
+      )
+      return Result.success()
+    }
+
+    return Result.success(await execute(options))
+  }
+}
